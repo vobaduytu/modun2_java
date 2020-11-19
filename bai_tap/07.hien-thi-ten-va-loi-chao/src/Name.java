4b3fbc0e800ba2a82b3ba9655ac73076b7143624
codeXploreRePo <vobaduytuok@gmail.com>
2020-11-19 15:42:38 +0700

    ok

---
+++ bai_tap/07.hien-thi-ten-va-loi-chao/src/Name.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = sc.nextLine();
+        System.out.println("hello:" + name);
+    }
+}
