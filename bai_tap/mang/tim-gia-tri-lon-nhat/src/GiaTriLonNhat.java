15a2e70dc78ff8aab6d0ef44606f4877a1a7a796
codeXploreRePo <vobaduytuok@gmail.com>
2020-11-23 14:52:17 +0700

    ok

---
+++ bai_tap/mang/tim-gia-tri-lon-nhat/src/GiaTriLonNhat.java
@@ -0,0 +1,13 @@
+public class GiaTriLonNhat {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("gia tri lon nhat");
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
