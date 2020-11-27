19c522ccee93f5073dcd3b6139b9f250b6e08ea8
codeXploreRePo <vobaduytuok@gmail.com>
2020-11-27 14:27:59 +0700

    ok

--- bai_tap/AbstractClass-Interface/hinh-hoc/src/Main.java
+++ bai_tap/AbstractClass-Interface/hinh-hoc/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ComparableCircle[] circles = {new ComparableCircle(6,"red"),new ComparableCircle(3,"blue"),new ComparableCircle(5,"white")};
+        ComparableCircle[] circles = {new ComparableCircle(6, "red"), new ComparableCircle(3, "blue"), new ComparableCircle(5, "white")};
 
         System.out.println(Arrays.toString(circles));
         Arrays.sort(circles);
