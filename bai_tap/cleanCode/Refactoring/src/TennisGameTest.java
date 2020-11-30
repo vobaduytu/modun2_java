e2b9490b0fa59ae49e6bab662b455ada9d03c063
codeXploreRePo <vobaduytuok@gmail.com>
2020-11-30 16:07:27 +0700

    ok

--- bai_tap/cleanCode/Refactoring/src/TennisGameTest.java
+++ bai_tap/cleanCode/Refactoring/src/TennisGameTest.java
@@ -68,14 +68,14 @@ public class TennisGameTest {
     @Test
     public void checkAllScores() {
         int highestScore = Math.max(this.player1Score, this.player2Score);
-        int m_score1 = 0;
-        int m_score2 = 0;
+        int scoreOne = 0;
+        int scoreTwo = 0;
         for (int i = 0; i < highestScore; i++) {
             if (i < this.player1Score)
-                m_score1 += 1;
+                scoreOne += 1;
             if (i < this.player2Score)
-                m_score2 += 1;
+                scoreTwo += 1;
         }
-        assertEquals(this.expectedScore, TennisGame.getScore("John", "Bill", m_score1, m_score2));
+        assertEquals(this.expectedScore, TennisGame.getScore("John", "Bill", scoreOne, scoreTwo));
     }
 }
