06c8d2c0a62b08a410f4979d9256c1e537f10fd9
codeXploreRePo <vobaduytuok@gmail.com>
2020-12-01 19:51:46 +0700

    ok

--- bai_tap/mang/demo/src/Them.java
+++ bai_tap/mang/demo/src/Them.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Them {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Them {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6, 6, 7, 6, 9};
+        int[]   arr1=new arr1[];
+        System.out.println("nhap so muon xoa: ");
+        Scanner sc = new Scanner(System.in);
+        int so = sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+
+            while (arr[i] == so) {
+                for (int j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.length - 1] = arr1[i];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- bai_tap/mang/duong-cheo/src/DuongCheo.java
+++ bai_tap/mang/duong-cheo/src/DuongCheo.java
@@ -1,9 +1,10 @@
 public class DuongCheo {
     public static void main(String[] args) {
         int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 100}
+                {1, 2, 3, 55},
+                {4, 5, 6, 44},
+                {7, 8, 100, 555},
+                {1, 2, 3, 4}
         };
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
