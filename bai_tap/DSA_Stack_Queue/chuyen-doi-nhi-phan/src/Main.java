41bde0b9c9eddba52bc6a86938de579ff3f4737d
codeXploreRePo <vobaduytuok@gmail.com>
2020-12-09 14:19:28 +0700

    ok

--- bai_tap/DSA_Stack_Queue/chuyen-doi-nhi-phan/src/Main.java
+++ bai_tap/DSA_Stack_Queue/chuyen-doi-nhi-phan/src/Main.java
@@ -13,8 +13,22 @@ public class Main {
         }
         while (!stack.isEmpty()) {
             System.out.print(stack.pop());
-        }
 
+        }
+        System.out.println(" ");
+        System.out.println("nhap so nhi phan muon chuyen");
+        int t = sc.nextInt();
+        String s = Integer.toString(t);
+        int sum = 0;
+       for (int i = 0; i < s.length();i++){
+
+           char srt = s.charAt(i);
+           int number = Integer.parseInt(String.valueOf(srt));
+           int aVinh = (int) (number*Math.pow(2,s.length()-1-i));
+           sum += aVinh;
+
+       }
+        System.out.print(sum);
 
     }
 }
